56fc4fc984eb7df880e6290187f6045a3b0dd63e
deakolt <alexandervaccaro@gmail.com>
2014-12-14 18:27:42 -0800

    version bump (0.3.2) for bugfix in event.rb

--- lib/bandsintown.rb
+++ lib/bandsintown.rb
@@ -14,7 +14,7 @@ require 'bandsintown/event'
 require 'bandsintown/venue'
 
 module Bandsintown
-  VERSION = '0.3.1'
+  VERSION = '0.3.2'
   class APIError < StandardError; end
   class << self
     # All Bandsintown API requests require an app_id parameter for identification.
